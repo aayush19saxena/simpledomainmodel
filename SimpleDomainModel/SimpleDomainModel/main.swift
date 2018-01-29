97eb82b18ae9329e41b48aadaca7bb3f1131d383
Aayush <aayush19saxena@gmail.com>
2018-01-28 18:04:50 -0800

    Completed money class

--- SimpleDomainModel/SimpleDomainModel/main.swift
+++ SimpleDomainModel/SimpleDomainModel/main.swift
@@ -28,11 +28,56 @@ public struct Money {
   public var currency : String
   
   public func convert(_ to: String) -> Money {
+    switch to {
+    case "USD":
+        switch self.currency {
+        case "GBP":
+            return Money(amount: Int(self.amount * 2), currency: "USD")
+        case "CAN":
+            return Money(amount: Int(self.amount * 4/5), currency: "USD")
+        case "EUR":
+            return Money(amount: Int(self.amount * 2/3), currency: "USD")
+        default: return self
+        }
+    case "GBP":
+        switch self.currency {
+        case "USD":
+            return Money(amount: Int(self.amount * 1/2), currency: "GBP")
+        case "CAN":
+            return Money(amount: Int(self.amount * 2/5), currency: "GBP")
+        case "EUR":
+            return Money(amount: Int(self.amount * 1/3), currency: "GBP")
+        default: return self
+        }
+    case "CAN":
+        switch self.currency {
+        case "USD":
+            return Money(amount: Int(self.amount * 5/4), currency: "CAN")
+        case "GBP":
+            return Money(amount: Int(self.amount * 5/2), currency: "CAN")
+        case "EUR":
+            return Money(amount: Int(self.amount * 5/6), currency: "CAN")
+        default: return self
+        }
+    case "EUR":
+        switch self.currency {
+        case "USD":
+            return Money(amount: Int(self.amount * 3/2), currency: "EUR")
+        case "GBP":
+            return Money(amount: Int(self.amount * 3), currency: "EUR")
+        case "CAN":
+            return Money(amount: Int(self.amount * 6/5), currency: "EUR")
+        default: return self
+        }
+    default: return self
+    }
   }
   
   public func add(_ to: Money) -> Money {
+    return Money(amount: self.convert(to.currency).amount + to.amount, currency: to.currency)
   }
   public func subtract(_ from: Money) -> Money {
+    return Money(amount: self.amount - from.convert(self.currency).amount, currency: self.currency)
   }
 }
 
